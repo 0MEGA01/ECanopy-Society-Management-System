66041c1ceba5e1821fec011d242df1f3a770d907
Soumya Prasad Sahu <92383017+0MEGA01@users.noreply.github.com>
2026-02-15 16:43:10 +0530

    Delete FRONTEND-SPRINGBOOT directory

--- FRONTEND-SPRINGBOOT/src/services/accessService.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-export const accessService = {
-    validateQr: async (token) => {
-        const response = await api.post('/access/validate-qr', { token });
-        return response.data;
-    }
-};
--- FRONTEND-SPRINGBOOT/src/services/residentService.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-export const residentService = {
-    getDashboardStats: async () => {
-        const response = await api.get('/resident/dashboard-stats');
-        return response.data;
-    }
-};
