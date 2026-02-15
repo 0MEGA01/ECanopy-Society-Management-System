f5d40ffe55614dc0f5977bb4a95d6779f992d1ab
Soumya Prasad Sahu <92383017+0MEGA01@users.noreply.github.com>
2026-02-15 16:42:46 +0530

    Delete BACKEND-SPRINGBOOT directory

--- BACKEND-SPRINGBOOT/src/main/java/com/ecanopy/entity/enums/AccessType.java
+++
@@ -1,6 +0,0 @@
-package com.ecanopy.entity.enums;
-
-public enum AccessType {
-    ENTRY,
-    EXIT
-}
--- BACKEND-SPRINGBOOT/src/main/java/com/ecanopy/exception/ForbiddenException.java
+++
@@ -1,9 +0,0 @@
-package com.ecanopy.exception;
-
-public class ForbiddenException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    
-    public ForbiddenException(String message) {
-        super(message);
-    }
-}
